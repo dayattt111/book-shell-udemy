a93ac25ff72d148189ea2824bed4dcf706b6d12e
dayattt111 <tumbaldaya@gmail.com>
2025-09-02 21:26:51 +0800

    improve: logic DOM

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
+    // if(isChecked){
+    //     alert('Trueee');
+    //     const todoObject = generateTodoObject(generatedID, titleBook, authBook, yearBook, isChecked);
+    //     books.push(todoObject);
+    // } else{
+        //     alert('Falsee');
+        // };
+        // console.log(isChecked);
+        
 const books = [];
 const RENDER_EVENT = 'render-book';
 
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('bookForm');
     submitForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    addBook();
+        event.preventDefault();
+        addBook();
     });
 });
-
+        
 function generateId() {
-    return +new Date();
-}
-    
+        return +new Date();
+    }
+        
+        // console.log(books);
 function generateTodoObject(id, title, author, year, isCompleted) {
     return {
-    id,
-    title,
-    author,
-    year,
-    isCompleted,
+        id,
+        title,
+        author,
+        year,
+        isCompleted,
     };
-};
-
-// console.log(books);
+}
 
 document.addEventListener(RENDER_EVENT, function () {
     console.log(books);
 });
-
 
 function addBook() {
     const titleBook = document.getElementById('bookFormTitle').value;
@@ -36,79 +43,66 @@ function addBook() {
     const yearBook = document.getElementById('bookFormYear').value;
 
     const checkBook = document.getElementById('bookFormIsComplete');
-    var isChecked = checkBook.checked;
+    let isCompleted = checkBook.checked; 
 
     const generatedID = generateId();
-    const todoObject = generateTodoObject(generatedID, titleBook, authBook, yearBook, isChecked);
+    const todoObject = generateTodoObject(generatedID, titleBook, authBook, yearBook, isCompleted);
+
     books.push(todoObject);
-    
+
     document.dispatchEvent(new Event(RENDER_EVENT));
-
-    // return todoObject;
-};
-
+}
 
 function makeBook(todoObject) {
-
-    // console.log(books);
-
     const textTitle = document.createElement('h3');
     textTitle.innerText = todoObject.title;
 
     const textAuthor = document.createElement('p');
-    textAuthor.innerText = todoObject.author;
+    textAuthor.innerText = "Penulis: " + todoObject.author;
 
     const textYear = document.createElement('p');
-    textYear.innerText = todoObject.year;
+    textYear.innerText = "Tahun: " + todoObject.year;
 
     const buttonS = document.createElement('button');
     const buttonC = document.createElement('button');
     const buttonD = document.createElement('button');
 
-
     buttonS.classList.add('bookItemIsCompleteButton');
     buttonC.classList.add('bookItemDeleteButton');
     buttonD.classList.add('bookItemEditButton');
 
-// optional: text untuk tiap tombol
     buttonS.innerText = "Selesai dibaca";
     buttonC.innerText = "Edit Buku";
     buttonD.innerText = "Hapus Buku";
-    // function classListt(){
-    //     return
-    // };
+
+    const actionContainer = document.createElement('div');
+    actionContainer.append(buttonS, buttonC, buttonD);
 
     const textContainer = document.createElement('div');
     textContainer.classList.add('inner');
-    textContainer.append(textTitle, textAuthor, textYear, buttonS, buttonC, buttonD);
+    textContainer.append(textTitle, textAuthor, textYear, actionContainer);
 
     const container = document.createElement('div');
-
     container.classList.add('item', 'shadow');
     container.append(textContainer);
-    container.setAttribute('id', 'book-${todoObject.id}');
+    container.setAttribute('id', `book-${todoObject.id}`);
 
     return container;
-};
+}
 
 document.addEventListener(RENDER_EVENT, function () {
-    //   console.log(bookItem);
-      const uncompletedBookList = document.getElementById('bookItems');
-      const completedBookList = document.getElementById('completeBookList');
-      uncompletedBookList.innerHTML = '';
-    //   completedBookList.innerHTML = '';
-     
+    const incompleteBookList = document.getElementById('incompleteBookList');
+    const completedBookList = document.getElementById('completeBookList');
 
-    // console.log(uncompletedBookList);
-    // console.log(completedBookList);
+    incompleteBookList.innerHTML = '';
+    completedBookList.innerHTML = '';
 
-    // if(uncompletedBookList === false){
-        for (const bookItem of books) {
-          const bookElement = makeBook(bookItem);
-          if(!bookItem === isChecked){
-              uncompletedBookList.append(bookElement);
-          }
+    for (const bookItem of books) {
+        const bookElement = makeBook(bookItem);
+        if (!bookItem.isCompleted) {
+            incompleteBookList.append(bookElement);
+        } else {
+            completedBookList.append(bookElement);
         }
-    // }
-
+    }
 });
